ffed87cc50414295622ec8662d89eea91f9c4c63
T540\Anton <aatonkushin@gmail.com>
2019-05-05 11:57:13 +0500

    Создал проект

---
+++ src/main/java/org/tonkushin/hw09/Hw09Application.java
@@ -0,0 +1,13 @@
+package org.tonkushin.hw09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw09Application.class, args);
+	}
+
+}
